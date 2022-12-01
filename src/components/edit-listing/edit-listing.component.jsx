c4c2dc17c58a49dd407252e4967b174a18db4a72
sanyvukl <80652443+sanyvukl@users.noreply.github.com>
2022-12-01 00:41:05 -0800

    Update edit-listing.component.jsx

--- src/components/edit-listing/edit-listing.component.jsx
+++ src/components/edit-listing/edit-listing.component.jsx
@@ -6,7 +6,7 @@ import Spinner from "../Spinner/spinner.component";
 import { toast } from "react-toastify";
 import { getStoreImagesUrl } from "../../utils/firebase/firebase.utils";
 import { serverTimestamp } from "firebase/firestore";
-import config from "../../config.json";
+// import config from "../../config.json";
 
 const EditListing = () => {
     const params = useParams();
@@ -88,7 +88,7 @@ const EditListing = () => {
         if (geolocationEnabled) {
             const responce = await fetch(
                 `https://maps.googleapis.com/maps/api/geocode/json?address=${address}
-                 &key=${config.GOOGLE_MAPS_API_KEY}`);
+                 &key=AIzaSyCjtfnkdDwh4I7yR1CV77cXIk5ljioYVxw`);
             const data = await responce.json();
             geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
             geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
